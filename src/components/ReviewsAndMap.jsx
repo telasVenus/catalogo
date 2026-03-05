9aaaf0def27920b23da7a5f88ff908c84fa669a8
Rhaissa Marques <rhaissa.marques@gmail.com>
2026-03-05 17:06:42 -0300

    hiding float whatsapp button when in mobile

--- src/components/ReviewsAndMap.jsx
+++ src/components/ReviewsAndMap.jsx
@@ -23,7 +23,7 @@ export default function ReviewsAndMap() {
           Empresa sob nova direção desde 2025. Compromisso renovado com a excelência.
         </p>
         <iframe 
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d930.0513226408939!2d-43.9051702546532!3d-19.60493264705501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa69bd43cb50c0f%3A0xff1c20680a5f6d5e!2sF%C3%A1brica%20de%20Telas%20V%C3%AAnus!5e1!3m2!1spt-BR!2sbr!4v1752502110514!5m2!1spt-BR!2sbr" width="450"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d930.0513226408939!2d-43.9051702546532!3d-19.60493264705501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa69bd43cb50c0f%3A0xff1c20680a5f6d5e!2sF%C3%A1brica%20de%20Telas%20V%C3%AAnus!5e1!3m2!1spt-BR!2sbr!4v1752502110514!5m2!1spt-BR!2sbr" width="100%"
           height="350"
           style={{ border: 0 }}
           allowFullScreen
